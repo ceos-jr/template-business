e5ce446aa845bed28af637157e8e22ea4d4f2482
taldoflemis <coderflemis@gmail.com>
2023-02-02 13:38:01 -0300

    add font roboto as default font and next font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["var(--font-roboto)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
