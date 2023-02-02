cd443eb0c5457f1130a316c013222524d5ee2820
Luiz Gustavo <gustavo1204.gc@gmail.com>
2023-02-02 16:27:00 -0300

    create primary color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
       fontFamily: {
         inter: ["var(--font-inter)", ...fontFamily.sans],
       },
+      colors: {
+        primary: {
+          100: "#e6eedd",
+          200: "#ccddbb",
+          300: "#b3cd98",
+          400: "#99bc76",
+          500: "#80ab54",
+          600: "#668943",
+          700: "#668943",
+          800: "#4d6732",
+          900: "#334422",
+        }
+      },
     },
   },
   plugins: [],
