8f8a6db4ae45f422ab40b39b9f0ad7479c376445
Luiz Gustavo <gustavo1204.gc@gmail.com>
2023-02-03 22:55:35 -0300

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="absolute top-0 w-full h-20 bg-black md:top-12 lg:h-24">
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
