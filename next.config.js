0085bd438206c0c740e476d5ae353aaee4820c47
taldoflemis <coderflemis@gmail.com>
2023-02-05 14:46:51 -0300

    add next optimized image exporter

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["templatemo.com"],
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 3840],
+  },
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
   },
 };
