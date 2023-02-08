ca44ec0bc140035096dd2c8cab54c2a899e9dc44
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-08 09:03:27 -0300

    Footer minor adjustments
    (Without responsiveness yet)

--- components/Footers/Footer3.tsx
+++ components/Footers/Footer3.tsx
@@ -12,8 +12,10 @@ import {
 const Footer3 = () => {
     return(
         <footer className={styles.footer}>
-                <div className="first-section">
-                    <FaCube size="10rem"/>
+
+                <div className={styles.firstsection}>
+                <FaCube size="10rem"/>
+                <span>Contact us</span>
                     <ul>
                         <li>(85) 7777 7777</li>
                         <li>ceosadmin@gmail.com</li>
@@ -24,29 +26,33 @@ const Footer3 = () => {
 
                 <div className={styles.box}>
                     {/*Quero dividir isso em mais colunas com info */}
-                    <h3>Products</h3>
                     <ul>
-                        <li></li>
-                        <li></li>
+                        <h3>Products</h3>
+                        <li>Overview</li>
+                        <li>Features</li>
+                        <li>Pricing</li>
+                        <li>Releases</li>
+                    </ul>
+                    <ul>
+                        <h3>Resources</h3>
+                        <li>blog</li>
+                        <li>events</li>
+                        <li>support</li>
+                    </ul>
+                    
+                    <ul>
+                        <h3>Company</h3>
+                        <li>About us</li>
+                        <li>News</li>
                         <li></li>
                     </ul>
-                    <h3>Company</h3>
+                    
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <h3>Resources</h3>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <h3>Legal</h3>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <h3>Legal</h3>
+                        <li>Terms</li>
+                        <li>Privacy</li>
+                        <li>Licenses</li>
+                        <li>Settings</li>
                     </ul>
                 </div>
                 -
