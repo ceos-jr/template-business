7015f7a355d860815b04c6b60358c833979cfba2
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-10 15:38:31 -0300

    Começando de novo

---
+++ src/components/Footers/Footer5.tsx
@@ -0,0 +1,67 @@
+import styles from "./Footer5.module.css"
+import {
+  FaCube,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa"
+
+const Footer5 = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.firstsection}>
+        <FaCube size="15rem" />
+        <div className={styles.contato}>
+          <ul>
+            <h3>Contact us</h3>
+            <li>(85) 7777 7777</li>
+            <li>ceosadmin@gmail.com</li>
+            <li>Campus do Pici - UFC, Fortaleza, CE</li>
+            <br></br>
+            <li>Copyright © 2023 Financial Business</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <ul>
+          <h3>Products</h3>
+          <li>Overview</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Releases</li>
+        </ul>
+        <ul>
+          <h3>Resources</h3>
+          <li>Blog</li>
+          <li>Events</li>
+          <li>Support</li>
+        </ul>
+
+        <ul>
+          <h3>Company</h3>
+          <li>About us</li>
+          <li>News</li>
+          <li></li>
+        </ul>
+
+        <ul>
+          <h3>Legal</h3>
+          <li>Terms</li>
+          <li>Privacy</li>
+          <li>Licenses</li>
+          <li>Settings</li>
+        </ul>
+      </div>
+      -
+      <div className="socials">
+        <FaInstagram size="7rem" />
+        <FaFacebookF size="7rem" />
+        <FaLinkedin size="7rem" />
+        <FaWhatsapp size="7rem" />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer5
