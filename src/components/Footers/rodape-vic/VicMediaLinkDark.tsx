04f5f770b0adba9a6508e70487ba3af75f3d72fd
viquecastro <victoriacastro3108@gmail.com>
2023-02-10 10:28:09 -0300

    Removendo erro no arquivo VicMedialinkdark

--- src/components/Footers/rodape-vic/VicMediaLinkDark.tsx
+++ src/components/Footers/rodape-vic/VicMediaLinkDark.tsx
@@ -3,7 +3,7 @@ import { IconType } from "react-icons"
 const VicMediaLinkDark = ({ link, Icon }: { link: string; Icon: IconType }) => {
   return (
     <a href={link}>
-      <Icon className="p-1 rounded-lg transition-colors duration-300 cursor-pointer hover:text-gray-900 text-rose-900 hover:text-sky-600" />
+      <Icon className="p-1 rounded-lg transition-colors duration-300 cursor-pointer text-rose-900 hover:text-sky-600" />
     </a>
   )
 }
