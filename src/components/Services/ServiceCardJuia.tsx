5439a50528c76a171d4577f11c60f6ca1792ebfa
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-14 18:09:29 -0300

    Adição dos arquivos ServiceCardJuia e ServicesJuia

---
+++ src/components/Services/ServiceCardJuia.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import { ServiceJuiaType } from "./ServicesJuia" {/*?*/}
+
+type ServiceCardJuiaProps = ServiceJuiaType
+
+const ServiceCardJuia = ({name, image, description}: ServiceCardJuiaProps) => {
+    return(
+
+    )
+}
+
+export default ServiceCardJuia
---
+++ src/components/Services/ServicesJuia.tsx
@@ -0,0 +1,20 @@
+import ServiceCardJuia from "./ServiceCardJuia"
+import { StaticImageData } from "next/image"
+
+export type ServiceJuiaType =  {
+    name: string
+    image: StaticImageData 
+    description: string
+}
+{/*primeiro to tentando entender oq rola aqui*/}
+
+const services: ServiceJuiaType[] = [
+   
+]
+
+const ServicesJuia = () => {
+    return (
+        
+    )
+}
+export default ServicesJuia 
