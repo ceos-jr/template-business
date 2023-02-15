e1d74b3f0c63f8685348f84bc3f600919883c46c
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-15 17:30:37 -0300

    services juia pronto

--- src/components/Services/ServicesJuia.tsx
+++ src/components/Services/ServicesJuia.tsx
@@ -1,75 +1,67 @@
 import Image from "next/image"
+import { StaticImageData } from "next/image"
+import { Service1Type } from "./Services1"
 
-{
-  /*nao gostei da fonte branca do texto mas nao sei oq ficaria melhor. Não tem como colocar sombra?*/
+
+type ServiceCardJuiaProps = Service1Type
+const ServiceCardJuia = ({name, image, description}:ServiceCardJuiaProps) => {
+  return(
+          <div className="w-full group">
+            <div className="h-64 rounded-lg relative overflow-hidden">
+              <Image
+                src={image}
+                alt={`service image for ${name}`}
+                fill
+                sizes="(max-width: 640px) 100vw,
+              (max-width: 1024px) 40vw,
+              (max-width: 1200px) 33vw,
+              350px"
+                className="object-cover group-hover:scale-125 transition-transform duration-1000"
+              />
+            </div>
+            <h3 className="w-full h-6 mt-4 bg-gray-200 rounded-lg group-hover:scale-105 group-hover:font-bold group-hover:text-teal-700 transition-all text-center tracking-wider group-hover:cursor-pointer">
+              {name}
+            </h3>
+            <p className="w-full p-4 mt-4 bg-gray-200 rounded-lg group-hover:scale-105 group-hover:text-teal-700 transition-all text-center tracking-wider group-hover:cursor-pointer">
+              {description}
+            </p>
+          </div>
+  )
 }
+
+const serviceList: ServiceCardJuiaProps[] = [
+  {
+    name: "Service 1.0",
+    description: "Vender barquinho",
+    image:"https://images.unsplash.com/photo-1575237402880-4b496a83ae04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8?=force=true"
+  },
+  {
+    name: "Service 2.0",
+    description: "Arrumar barquinho",
+    image:"https://images.unsplash.com/photo-1575237402880-4b496a83ae04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8?=force=true"
+  },
+  {
+    name: "Service 3.0",
+    description: "Comprar barquinho",
+    image:"https://images.unsplash.com/photo-1575237402880-4b496a83ae04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8?=force=true"
+  },
+  {
+    name: "Service 4.0",
+    description: "Alugar barquinho",
+    image:"https://images.unsplash.com/photo-1575237402880-4b496a83ae04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8?=force=true"
+  }
+]
 
 const ServicesJuia = () => {
   return (
-    <section className="bg-gradient-to-r from-zinc-200 to-slate-800">
-      <div className="container px-6 py-10 mx-auto">
-        <div className="grid grid-cols-1 gap-8 mb-12 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:gr-cols-3">
-          <div className="w-full">
-            <div className="w-full h-64 bg-gray-300 rounded-lg relative overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1575237402880-4b496a83ae04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8?=force=true"
-                alt="barquinho"
-                objetc-fit="contain"
-                fill
-                className="hover:scale-125"
-              />
-            </div>
-            <h1 className="w-56 h-6 mt-4 bg-gray-200 rounded-lg animate-pulse hover:scale-125 cursor-pointer hover:font-bold hover:text-teal-700">
-              <h4 className="text-center tracking-wider">Service 1.0</h4>
-            </h1>
-            <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg animate-pulse"></p>
-          </div>
-          <div className="w-full">
-            <div className="w-full h-64 bg-gray-300 rounded-lg relative overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1650845918019-19d14b959b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=?force=true"
-                alt="japon"
-                objetc-fit="contain"
-                fill
-                className="hover:scale-125"
-              />
-            </div>
-            <h1 className="w-56 h-6 mt-4 bg-gray-200 rounded-lg animate-pulse hover:scale-125 cursor-pointer hover:font-bold hover:text-orange-600">
-              <h4 className="text-center tracking-wider">Service 2.0</h4>
-            </h1>
-            <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg animate-pulse"></p>
-          </div>
-          <div className="w-full">
-            <div className="w-full h-64 bg-gray-300 rounded-lg relative overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1543710998-0b2b2a3c3a01?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto&force=true"
-                alt="muie"
-                objetc-fit="contain"
-                fill
-                className="hover:scale-125"
-              />
-            </div>
-            <h1 className="w-56 h-6 mt-4 bg-gray-200 rounded-lg animate-pulse hover:scale-125 cursor-pointer hover:font-bold hover:text-yellow-700">
-              <h4 className="text-center tracking-wider">Service 3.0</h4>
-            </h1>
-            <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg animate-pulse"></p>
-          </div>
-          <div className="w-full">
-            <div className="w-full h-64 bg-gray-300  rounded-lg relative overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1673997058976-497b8e85f3f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto&force=true"
-                alt="paisagem lixo"
-                objetc-fit="contain"
-                fill
-                className="hover:scale-125"
-              />
-            </div>
-            <h1 className="w-56 h-6 mt-4 bg-gray-200 rounded-lg animate-pulse hover:scale-125 cursor-pointer hover:font-bold">
-              <h4 className="text-center tracking-wider">Service 4.0</h4>
-            </h1>
-            <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg animate-pulse"></p>
-          </div>
-        </div>
+    <section className="bg-gray-300">
+      <div className="container px-6 py-10 mx-auto flex flex-col items-center space-y-8">
+        <h1 className="text-4xl font-bold tracking-wider text-gray-700" >Our Services</h1>
+        <div className="grid grid-cols-1 gap-8 mb-12 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 w-full">
+          {serviceList.map((service) => (
+            <ServiceCardJuia key={service.name} name={service.name} description={service.description} image={service.image}/>
+          ))}
+        </div> 
       </div>
     </section>
   )
