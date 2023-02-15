f5e11eb9eeeda777def0315f6da003a0f56f4e07
viquecastro <victoriacastro3108@gmail.com>
2023-02-15 19:56:20 -0300

    Imagens removidas da pasta Public, e mais alterações no arquivo VicServices

--- src/components/Services/VicServices.tsx
+++ src/components/Services/VicServices.tsx
@@ -1,7 +1,4 @@
 import VicServiceCard from "./VicServiceCard"
-import Vic1 from "@/images/services/vic1.webp"
-import Vic2 from "@/images/services/vic2.webp"
-import Vic3 from "@/images/services/vic3.webp"
 import { StaticImageData } from "next/image"
 
 export type Service1Type = {
@@ -12,24 +9,24 @@ export type Service1Type = {
 
 const services1: Service1Type[] = [
   {
-    name: "Servico 1",
-    image: Vic1,
+    name: "Feature 1",
+    image: "https://images.unsplash.com/photo-1650845918019-19d14b959b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=?force=true ",
   },
 
 ]
 
 const services2: Service1Type[] = [
   {
-    name: "Servico 2",
-    image: Vic2,
+    name: "Feature 2",
+    image: "https://images.unsplash.com/photo-1650845918019-19d14b959b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=?force=true ",
   },
 
 ]
 
 const services3: Service1Type[] = [
   {
-    name: "Servico 3",
-    image: Vic3,
+    name: "Feature 3",
+    image: "https://images.unsplash.com/photo-1650845918019-19d14b959b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=?force=true ",
   },
 
 ]
@@ -38,34 +35,42 @@ const services3: Service1Type[] = [
 
 const VicServices = () => {
   return (
-    <section className="container flex flex-col gap-y-16 bg-amber-100 justify-center items-center mx-auto mt-24 divide-y divide-black md:py-8 ">
+    <section className="container flex flex-col gap-y-16 bg-stone-200	 justify-center items-center mx-auto mt-24 divide-y divide-black md:py-8 ">
       <div>
         <h1 className="text-4xl font-bold text-gray-800">
-          Nossos <span className="text-primary-500">Servicos</span>
+          Our <span className="text-red-800	">Features</span>
         </h1>
         <p className="pt-4 font-light text-gray-800 uppercase">
           Adquira um dos nossos servicos
         </p>
       </div>
       
-      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-3 ">
+      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-2 ">
         {services1.map((service) => (
           <VicServiceCard key={service.name} {...service} />
         ))}
+        <div>
+          <p className="text-xl">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi corrupti delectus et natus officia? Delectus, aliquid vero sunt voluptatem saepe voluptate quis amet, necessitatibus officia fugiat molestias earum dignissimos aspernatur."</p>
+        </div>
       </div>
       
-      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-2">
         {services2.map((service) => (
           <VicServiceCard key={service.name} {...service} />
         ))}
+        <div>
+          <p className="text-xl">"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam officiis et amet beatae eveniet voluptatem autem consectetur quidem hic neque architecto molestiae, harum nemo distinctio omnis recusandae ad assumenda delectus!"</p>
+        </div>
       </div>
       
-      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 p-12 w-full md:grid-cols-2">
         {services3.map((service) => (
           <VicServiceCard key={service.name} {...service} />
         ))}
+        <div>
+          <p className="text-xl">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, id? Laboriosam harum quis aliquid fugiat iure voluptatem praesentium culpa accusamus! Eaque ea unde voluptatem cum placeat quia necessitatibus labore neque!"</p>
+        </div>
       </div>
-
     </section>
   )
 }
