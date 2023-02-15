350731e664978a533ec5689541e4db3ecfee5fb7
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-14 23:00:51 -0300

    Remocao do ServiceCardJuia, display basico do componente service, alteracao do index para visualizacao da pagina, problemas com importacao de imagem

--- src/components/Services/ServicesJuia.tsx
+++ src/components/Services/ServicesJuia.tsx
@@ -1,20 +1,30 @@
-import ServiceCardJuia from "./ServiceCardJuia"
-import { StaticImageData } from "next/image"
+import Image from "next/image"
 
-export type ServiceJuiaType =  {
-    name: string
-    image: StaticImageData 
-    description: string
-}
-{/*primeiro to tentando entender oq rola aqui*/}
-
-const services: ServiceJuiaType[] = [
-   
-]
+{/*nao gostei da fonte branca do texto mas nao sei oq ficaria melhor. Não tem como colocar sombra?*/}
 
 const ServicesJuia = () => {
-    return (
-        
-    )
+  return (
+    <section className="bg-gradient-to-r from-zinc-200 to-slate-800">
+        <h1 className="text-white text-center py-12 text-4xl font-extrabold cursor-pointer duration-400">Our Services</h1>
+      <div className="container px-6 py-10 mx-auto animate-pulse">
+        <div className="grid grid-cols-1 gap-8 mb-12 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:gr-cols-3">
+          <div className="w-full">
+            <div className="w-full h-64 bg-gray-300  rounded-lg">
+                {/*aqui vai a imagem se der certo*/}
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="w-full h-64 bg-gray-300 rounded-lg"></div>
+          </div>
+          <div className="w-full">
+            <div className="w-full h-64 bg-gray-300  rounded-lg"></div>
+          </div>
+          <div className="w-full">
+            <div className="w-full h-64 bg-gray-300  rounded-lg"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
-export default ServicesJuia 
+export default ServicesJuia
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Head from "next/head"
 import SubHeader1 from "@/components/Nav-Header/SubHeader1"
 import Header1 from "@/components/Nav-Header/Header1"
 import Hero1 from "@/components/Heros/Hero1"
-import Services1 from "@/components/Services/Services1"
+import ServicesJuia from "@/components/Services/ServicesJuia"
 import StatisticsSection from "@/components/Stats/Stats1"
 import Member1 from "@/components/Members/Members1"
 import Clients1 from "@/components/Clients/Clients1"
 import Contact1 from "@/components/Contact/Contact1"
-import Footer6 from "@/components/Footers/Footer6"
+import Footer5 from "@/components/Footers/Footer5"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,12 +28,12 @@ const Home: NextPage = () => {
       <Header1 />
       <main className="w-full min-h-screen">
         <Hero1 />
-        <Services1 />
+        <ServicesJuia />
         <StatisticsSection />
         <Clients1 />
         <Member1 />
         <Contact1 />
-        <Footer6 />
+        <Footer5 />
       </main>
     </div>
   )
