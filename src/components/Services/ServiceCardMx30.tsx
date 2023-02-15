2f2d7ca53e011d5655d20baa33ebb939162fd3b7
MarceloMx30 <marcelomeirelesmarquesfilho@hotmail.com>
2023-02-15 12:15:09 -0300

    Terminada a parte de services do mx30

--- src/components/Services/ServiceCardMx30.tsx
+++ src/components/Services/ServiceCardMx30.tsx
@@ -4,19 +4,22 @@ type ServiceCardMx30Props = Service1Type
 
 const ServiceCard = ({ name, image, description }: ServiceCardMx30Props) => {
   return (
-    <div className="flex overflow-hidden flex-col items-center bg-zinc-800 rounded-lg shadow-xl min-h-64">
+    <div className="group flex overflow-hidden flex-col items-center bg-zinc-800 rounded-md shadow-md shadow-violet-800 min-h-64 hover:shadow-lg hover:shadow-violet-700 ">
       <div className="overflow-hidden relative w-full aspect-video">
         <Image
           src={image}
           alt={`service ${name}`}
           className="object-cover"
-          placeholder="blur"
-          sizes="(max-width: 768px) 80vw, 350px"
+          placeholder="empty"
           fill
+          sizes="(max-width: 768px) 100vw,
+          20vw"
         />
       </div>
       <div className="p-6">
-        <h1 className="text-2xl font-bold text-white">{name}</h1>
+        <h1 className="text-2xl font-bold text-white group-hover:text-violet-800">
+          {name}
+        </h1>
         <p className="text-gray-400">{description}</p>
       </div>
     </div>
