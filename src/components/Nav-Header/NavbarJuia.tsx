dfbd0f5a0c209c5f257647ab8c271b918ba17247
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-23 08:51:15 -0300

    alterações navbarjuia tsx e css

--- src/components/Nav-Header/NavbarJuia.tsx
+++ src/components/Nav-Header/NavbarJuia.tsx
@@ -1,41 +1,46 @@
-import React, {useState} from 'react'
-import {FaCat, FaBars, FaTimes} from 'react-icons/fa'
+import styles from "./NavbarJuia.module.css"
+import React, { useState } from "react"
+import { FaCat, FaBars, FaTimes } from "react-icons/fa"
 
 const NavbarJuia = () => {
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
 
-    const[click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const closeMenu = () => setClick(false)
 
-    const closeMenu = () => setClick(false)
-
-    return (
-        <div className="max-w-full bg-white"> {/*header*/}
-            <nav>
-                <FaCat className="text-3xl"/>
-                <div className="onClick={handleClick}">
-                    {click ?  (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
-                </div>
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    <li className="m-6">
-                        <a href="/" onClick={closeMenu}>Home</a>
-                    </li>
-                    <li className="m-6">
-                        <a href="#services" onClick={closeMenu}>Our services</a>
-                    </li>
-                    <li className="m-6">
-                        <a href="#about" onClick={closeMenu}>About us</a>
-                    </li>
-                    <li className="m-6">
-                        <a href="#footer" onClick={closeMenu}>Contact us</a>
-
-                    </li>
-
-                </ul>
-            </nav>
-            
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">
+          <FaCat />
+        </a>
+        <div className="hamburguer" onClick={handleClick}>
+          {click ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
-    )
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a href="/" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#services" onClick={closeMenu}>
+              Our services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" onClick={closeMenu}>
+              About us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#footer" onClick={closeMenu}>
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
-
 export default NavbarJuia
