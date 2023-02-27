7351538b7666703cec7565e96d62091ba60aff12
Ellen Kaori <julianaomitakimaki@gmail.com>
2023-02-27 18:48:26 -0300

    deixei o head certo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SubHeader1 />
-  <Header1 /> 
+
       <NavbarJuia />
       <main className="w-full min-h-screen">
         <Hero1 />
