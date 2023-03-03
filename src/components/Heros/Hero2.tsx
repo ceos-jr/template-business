15187b00efac3dbbddb82140cd8635f190af2313
MarceloMx30 <marcelomeirelesmarquesfilho@hotmail.com>
2023-03-03 07:54:16 -0300

    add texto no Hero

--- src/components/Heros/Hero2.tsx
+++ src/components/Heros/Hero2.tsx
@@ -9,9 +9,10 @@ import { Autoplay, Pagination, Navigation } from "swiper"
 interface SlideProps {
   name: string
   image: string
+  text: string
 }
 
-const Slide = ({ name, image }: SlideProps) => {
+const Slide = ({ name, image, text }: SlideProps) => {
   return (
     <Image
       src={image}
@@ -28,11 +29,13 @@ const List: SlideProps[] = [
     name: "image1",
     image:
       "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&dl=campbell-3ZUsNJhi_Ik-unsplash.jpg&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    text: "SAID CARROS🚓",
   },
   {
     name: "image2",
     image:
       "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&dl=florian-olivo-4hbJ-eymZ1o-unsplash.jpg&w=1920&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    text: "SAID CODIGOS☢",
   },
 ]
 
@@ -62,7 +65,12 @@ const Hero2 = () => {
             key={item.name}
             className="flex overflow-hidden relative justify-center items-center w-full h-full text-lg text-center bg-zinc-800 aspect-video"
           >
-            <Slide name={item.name} image={item.image} />
+            <Slide name={item.name} image={item.image} text={item.text} />
+            <div className="absolute top-3/4 left-1/4 transform -translate-x-2/3 w-4/5 sm:w-3/5 lg:w-1/2 xl:w-2/5 2xl:w-1/3 h-auto bg-gradient-to-r from-purple-900 to-zinc-800 opacity-80 rounded-lg shadow-md text-center">
+              <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl py-4 drop-shadow-lg text-white">
+                {item.text}
+              </p>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from "next"
 import { Inter } from "@next/font/google"
 import Head from "next/head"
-import Hero3 from "@/components/Heros/Hero3"
+import Hero2 from "@/components/Heros/Hero2"
 import StatisticsSection from "@/components/Stats/Stats1"
 import Member1 from "@/components/Members/Members1"
 import Clients1 from "@/components/Clients/Clients1"
 import Contact1 from "@/components/Contact/Contact1"
 import Footer7 from "@/components/Footers/Footer7"
 import Services3 from "@/components/Services/Services3"
-import Navbar2 from "@/components/Nav-Header/NavBar2"
+import Navbar3 from "@/components/Nav-Header/Navbar3"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,8 +24,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full min-h-screen">
-        <Navbar2 />
-        <Hero3 />
+        <Navbar3 />
+        <Hero2 />
         <Services3 />
         <StatisticsSection />
         <Clients1 />
