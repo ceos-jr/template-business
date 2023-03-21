c4767278524c387f32661f2109078482889d23f5
Lucas Braide <107264197+lucasbraide@users.noreply.github.com>
2023-03-20 22:38:47 -0300

    hero lucas faltando otimização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import type { NextPage } from "next"
 import { Inter } from "@next/font/google"
 import Head from "next/head"
+import Hero from "@/components/Heros/Hero1"
+import Hero2 from "@/components/Heros/Hero2"
 import Hero3 from "@/components/Heros/Hero3"
 import StatisticsSection from "@/components/Stats/Stats1"
 import Member1 from "@/components/Members/Members1"
 import Clients1 from "@/components/Clients/Clients1"
 import Contact1 from "@/components/Contact/Contact1"
-import Footer7 from "@/components/Footers/Footer7"
+import {
+  Footer1,
+  Footer2,
+  Footer3,
+  Footer4,
+  Footer5,
+  Footer6,
+  Footer7,
+} from "@/components/Footers"
 import Services3 from "@/components/Services/Services3"
 import Navbar2 from "@/components/Nav-Header/NavBar2"
+import NavBar4 from "@/components/Nav-Header/NavBar4"
+import Services4 from "@/components/Services/Sevices4"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,15 +36,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full min-h-screen">
-        <Navbar2 />
+        <NavBar4 />
         <Hero3 />
-        <Services3 />
+        <Services4 />
         <StatisticsSection />
         <Clients1 />
         <Member1 />
         <Contact1 />
       </main>
-      <Footer7 />
+      <Footer4 />
     </div>
   )
 }
