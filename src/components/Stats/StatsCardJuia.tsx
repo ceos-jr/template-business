1dcd90ea82dca60d8f35dea00c80f89d4a3cfc38
Julia Naomi <julianaomitakimaki@gmail.com>
2023-03-24 19:31:50 -0300

    criacao pagina juia.tsx

--- src/components/Stats/StatsCardJuia.tsx
+++ src/components/Stats/StatsCardJuia.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { StatsType } from "./Statsjuia"
+import { StatsType } from "./Stats4"
 import { useEffect, useState, useRef } from "react"
 
 type CardProps = StatsType
@@ -33,8 +33,9 @@ const CardJuia = ({
   }, [visible, value])
 
   return (
-    <div className={`flex flex-col gap-4 items-center p-4 font-bold rounded-lg shadow-xl min-h-64  bg-yellow-200 transition-all ease-in-out duration-1000 ${className}`}
-    style={{ transitionDelay: delay }}
+    <div
+      className={`flex flex-col gap-4 items-center p-4 font-bold rounded-lg shadow-xl min-h-64  bg-yellow-200 transition-all ease-in-out duration-1000 ${className}`}
+      style={{ transitionDelay: delay }}
     >
       <h1 className="text-5xl text-pink-600">{value}</h1>
       <h1 className="text-gray-500">{text}</h1>
--- src/pages/juia.tsx
+++ src/pages/juia.tsx
@@ -2,19 +2,18 @@ import type { NextPage } from "next"
 import { Inter } from "@next/font/google"
 import Head from "next/head"
 import Hero3 from "@/components/Heros/Hero3"
-import StatisticsSection from "@/components/Stats/Stats4"
 import Member1 from "@/components/Members/Members1"
 import Clients1 from "@/components/Clients/Clients1"
 import Contact1 from "@/components/Contact/Contact1"
 import Footer7 from "@/components/Footers/Footer7"
 import Services3 from "@/components/Services/Services3"
 import Navbar3 from "@/components/Nav-Header/Navbar3"
-import Navbar2 from "@/components/Nav-Header/NavBar2"
+import StatisticsSection from "@/components/Stats/Stats4"
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-})
+    subsets: ["latin"],
+    variable: "--font-inter",
+  })
 
 const Home: NextPage = () => {
   return (
@@ -25,10 +24,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full min-h-screen">
-        <Navbar2 />
+        <Navbar3 />
         <Hero3 />
         <Services3 />
-        <StatisticsSection />
+        <StatisticsSection/>
         <Clients1 />
         <Member1 />
         <Contact1 />
