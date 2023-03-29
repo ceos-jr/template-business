3b0ed0c2ca8e02c4b8dec5d34f0cc20a59b6f004
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-03-29 18:52:48 -0300

    Lindando com possiveis error de input do tipo email, Esse codigo apesar de estar funcionando nao contempla todos os possiveis erros de input.

--- src/components/Contact/Contact2.tsx
+++ src/components/Contact/Contact2.tsx
@@ -1,28 +1,84 @@
-import Button from "../layout/Button1"
-import { type } from "../Services/Sevices4"
-import { pages } from "../../../node_modules/next/dist/esm/server/render"
-function Input(props) {
+import { useState } from "react"
+
+function InputValidate(props) {
+  const errorStyle = props.error ? "border-rose-500" : "";
+
   return (
     <label className="grid grid-columns-1 justify-items-center w-3/4 my-3">
       <span className=" text-white w-3/4">{props.name}</span>
       <input
         type={props.type}
         name={props.name}
-        className="w-3/4 h-6 text-sky-1000 bg-blue-100 p-0.5 rounded-sm"
+        value={props.value}
+        onChange={(e) => props.setValue(e.target.value)}
+        className={
+          props.error ? 
+            "w-3/4 h-6 text-sky-1000 bg-blue-100 p-0.5 rounded-sm active: outline-none border-2 border-rose-500" : 
+            "w-3/4 h-6 text-sky-1000 bg-blue-100 p-0.5 rounded-sm active: outline-none"
+        }
       ></input>
     </label>
   )
 }
 
+function Input(props) {
+  const erroInput = (<InputValidate 
+    type={props.type} 
+    name={props.name}
+    value={props.value}
+    setValue={props.setValue}
+    error={true}
+  />);
+  const okInput = (<InputValidate 
+    type={props.type} 
+    name={props.name}
+    value={props.value}
+    setValue={props.setValue}
+    error={false}
+  />);
+
+  switch (props.type) {
+    case "email":
+      if (props.value.search("@") && props.value.search(".")) {
+        return okInput;
+      } else {
+        return erroInput;
+      }
+      break;
+  
+    default:
+      return okInput;
+      break;
+  }
+}
+
+function handleOnChange(e, setter) {
+  setter(e.target.value)
+}
+
 const Contact2 = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState(0);
+  const [desc, setDesc] = useState("");
+
   return (
     <section className="container flex bg-blue-300 py-4 rounded-lg justify-center m-5">
       <div className="flex justify-around content-center bg-slate-700 w-3/4 py-5 min-h-50">
         <form className="grid justify-items-center w-5/12">
-          <Input name="name" type="text" />
-          <Input name="email" type="email" />
-          <Input name="number" type="number" />
-          <Input name="descripition" type="text" />
+          <Input name="name" type="text" value={name} setValue={setName} />
+          <Input name="email" type="email" value={email} setValue={setEmail} />
+          <Input name="number" type="number" value={number} setValue={setNumber} />
+          <label className="grid grid-columns-1 justify-items-center w-3/4 my-3">
+            <span className=" text-white w-3/4">description</span>
+            <textarea
+              value={desc}
+              className="w-3/4 text-sky-1000 bg-blue-100 p-0.5 rounded-sm resize-none"
+              rows={5}
+              maxLength={256}
+              onChange={(e) => setDesc(e.target.value)}
+            ></textarea>
+          </label>
           <button type="submit">Submit</button>
         </form>
         <div className="grid justify-items-center content-center w-5/12 min-h-70">
@@ -37,7 +93,7 @@ const Contact2 = () => {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Contact2
