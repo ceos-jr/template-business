ca29062602d46a986c086ad14f4182418c4d9bb2
MarceloMx30 <marcelomeirelesmarquesfilho@hotmail.com>
2023-03-31 14:48:47 -0300

    feat: adicionar o Member3 e MemberCard3
    atualizacao no page mx30 e criacao do members section e member card2

--- src/components/Clients/ClientCard2.tsx
+++ src/components/Clients/ClientCard2.tsx
@@ -2,7 +2,7 @@ import { Client2Type } from "./Clients2"
 type ClientCard2Props = Client2Type
 const ClientCard2 = ({ name, role, testimony }: ClientCard2Props) => {
   return (
-    <div className="p-5 pb-10 text-center bg-zinc-800 rounded-lg shadow-lg">
+    <div className="p-10 pb-10 text-center bg-zinc-800 rounded-lg shadow-lg">
       <h3 className="text-xl font-bold">{name}</h3>
       <h4 className="font-bold text-violet-800">{role}</h4>
       <p>{testimony}</p>
