0290eae659f77f9cb7034f14fe47b92ff2cdf96c
Julia Naomi <julianaomitakimaki@gmail.com>
2023-04-02 17:32:55 -0300

    mudanca para o swiper coverflow, falta alinhar o texto

--- src/components/Clients/ClientsJuia.tsx
+++ src/components/Clients/ClientsJuia.tsx
@@ -1,32 +1,75 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css/effect-coverflow"
+import "swiper/css/pagination"
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-cards";
-import { EffectCards } from "swiper";
+import { EffectCoverflow, Pagination } from "swiper"
 
- 
+import Image from "next/image"
+
+interface SlideProps {
+  titulo: string
+  img: string
+  texto: string
+}
+
+const List: SlideProps[] = [
+  {
+    titulo: "gay",
+    img: "https://cdn-icons-png.flaticon.com/512/763/763771.png",
+    texto: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  },
+  {
+    titulo: "gay2",
+    img: "https://cdn-icons-png.flaticon.com/512/763/763773.png",
+    texto: "The best services",
+  },
+  {
+    titulo: "gay3",
+    img: "https://cdn-icons-png.flaticon.com/512/8036/8036693.png",
+    texto: "lixuuu",
+  },
+]
+
 const ClientsJuia = () => {
-    return(
-        <section className="bg-white w-full h-screen mx-auto">
-        <Swiper
-        effect={"cards"}
+  return (
+    <section className="w-screen flex flex-wrap justify-center items-center bg-gray-100">
+      <h1 className="font-bold text-2xl m-5">What they say about us</h1>
+      <Swiper
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
-        modules={[EffectCards]}
-        slidesPerView={'auto'}
-
+        modules={[EffectCoverflow, Pagination]}
+        slidesPerView={"auto"}
+        className="h-1/2 flex items-center justify-center relative"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
       >
-        <SwiperSlide >
-          <div className= " bg-red-500 block max-w-sm rounded-lg p-10 m-20 ">
-          <h1>a</h1>
-          </div>
+        {List.map((item) => (
+          <SwiperSlide
+            key={item.titulo}
+            className="bg-pink-200 bg-center h-80 w-50 m-6 grid grid-cols-2 rounded-lg"
+          >
+            <Image
+              src={item.img}
+              alt={`${item.titulo} image`}
+              className="m-5"
+              width={250}
+              height={250}
+              priority
+            />
+            <div className="m-6 w-1/2 font-bold text-xl ">{item.texto}</div>
           </SwiperSlide>
-        <SwiperSlide className = "bg-blue-500 block max-w-sm rounded-lg p-10 m-20 ">b</SwiperSlide>
-        <SwiperSlide className = "bg-green-500 block max-w-sm rounded-lg p-10 m-20 ">c</SwiperSlide>
+        ))}
       </Swiper>
-      </section>
-    )
+    </section>
+  )
 }
 
 export default ClientsJuia
